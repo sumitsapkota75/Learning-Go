fbe7380fa994827a649204e5e87f56ee14c446e2
Sumit Sapkota <sumitsapkota0@gmail.com>
2021-04-05 22:43:38 +0545

    struct methods

--- main.go
+++ main.go
@@ -2,22 +2,36 @@ package main
 
 import "fmt"
 
-func changeValue(str *string) {
-	*str = "Changed"
+type Student struct {
+	name   string
+	grades []int
+	age    int
 }
 
-func changeValue2(str string) string {
-	str = "Changed"
-	print("from pointer", str)
-	return str
+func (s Student) getAge() int {
+	fmt.Println("AGE:", s.age)
+	return s.age
+}
+
+func (s *Student) setAge(age int) int {
+	fmt.Println("age from setAge 1", s.age)
+	s.age = age
+	fmt.Println("age from setAge", s.age)
+	return s.age
+}
+func (s Student) getAverageGrage() float32 {
+	sum := 0
+	for _, v := range s.grades {
+		sum += v
+	}
+	return float32(sum) / float32(len(s.grades))
 }
 
 func main() {
-	toChange := "Hello"
-	fmt.Println(toChange)
-	a := 7
-
-	changeValue2(toChange)
-	fmt.Println(&a)
-
+	s1 := &Student{name: "Sumit", grades: []int{1, 2, 3}, age: 21}
+	s1.getAge()
+	s1.setAge(44)
+	fmt.Println("Final age", s1.age)
+	avg := s1.getAverageGrage()
+	fmt.Println("Average", avg)
 }

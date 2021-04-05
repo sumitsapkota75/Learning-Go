046779157cadc6ae0b2e2799cf533102ada01e69
Sumit Sapkota <sumitsapkota0@gmail.com>
2021-04-05 22:06:09 +0545

    pointers and address

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+func changeValue(str *string) {
+	*str = "Changed"
+}
+
+func changeValue2(str string) string {
+	str = "Changed"
+	print("from pointer", str)
+	return str
+}
+
 func main() {
-	fmt.Println("Hello world")
+	toChange := "Hello"
+	fmt.Println(toChange)
+	a := 7
+
+	changeValue2(toChange)
+	fmt.Println(&a)
+
 }

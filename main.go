e1d8ef200d53522affec7b8c2cb9b6aebc06909a
Sumit Sapkota <sumitsapkota0@gmail.com>
2021-04-05 22:54:02 +0545

    interfaces

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+type shape interface {
+	area() float64
+}
+type Circle struct {
+	radius float64
+}
+
+func (c *Circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+type Rect struct {
+	height float64
+	width  float64
+}
+
+func (r *Rect) area() float64 {
+	return r.height * r.width
+}
+
+func getArea(s shape) float64 {
+	return s.area()
+}
 
 func main() {
-	fmt.Println("Hello world")
+	c1 := &Circle{4.6}
+	r1 := &Rect{2, 3}
+	shapes := []shape{r1, c1}
+
+	for _, shape := range shapes {
+		fmt.Println(getArea(shape))
+	}
 }
